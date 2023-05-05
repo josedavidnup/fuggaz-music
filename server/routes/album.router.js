d59acb894d540282942369d1a7e868e06fd8bfea
josedavidnup <josedavidnup@gmail.com>
2023-05-05 16:06:52 -0400

    genre and album routes and services

--- server/routes/album.router.js
+++ server/routes/album.router.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const {
+  getAlbum,
+  getAlbums,
+  createAlbum,
+  deleteAlbum,
+  updateAlbum,
+} = require('../services/album');
 
-router.post('/', () => {});
+router.get('/albums', getAlbums);
+router.get('/album/:id', getAlbum);
+router.post('/create-album', createAlbum);
+router.delete('/delete-album/:id', deleteAlbum);
+router.put('/update-album/:id', updateAlbum);
 
 module.exports = router;
